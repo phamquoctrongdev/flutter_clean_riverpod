69ac029fc4317b50ed39c06ba6b9d89a1b246eff
Pham Quoc Trong <pqtrong17@gmail.com>
2025-08-05 15:45:18 +0700

    Update dio dependency

--- lib/data/core/auth_interceptor.dart
+++ lib/data/core/auth_interceptor.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 
-class HeaderInterceptor extends Interceptor {
+class AuthInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     options.headers['x-api-key'] = 'reqres-free-v1';
--- lib/service/app_initializer.dart
+++ lib/service/app_initializer.dart
@@ -1,15 +1,11 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_clean_riverpod/data/core/api_client.dart';
-import 'package:flutter_clean_riverpod/data/core/api_endpoint.dart';
-import 'package:flutter_clean_riverpod/data/core/header_interceptor.dart';
+import 'package:flutter_clean_riverpod/data/core/dio_service.dart';
 import 'package:flutter_clean_riverpod/data/repository/country/country_repository.dart';
 import 'package:flutter_clean_riverpod/data/repository/country/country_repository_impl.dart';
 import 'package:flutter_clean_riverpod/service/connectivity_service.dart';
 import 'package:flutter_clean_riverpod/service/shared_preferences_service.dart';
 import 'package:get_it/get_it.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class AppInitializer {
   AppInitializer._internal();
@@ -19,7 +15,6 @@ class AppInitializer {
   static final getInstance = _instance;
 
   /// Ensure [ensureInitialized] function called once time.
-
   bool _isInitialized = false;
 
   Future<void> ensureInitialized() async {
@@ -40,32 +35,13 @@ class AppInitializer {
   }
 
   Future<void> _registerRepository() async {
-    final dio = _createDio();
-    final connectivityService = GetIt.instance.get<ConnectivityService>();
+    final dio = GetIt.instance.registerSingleton(DioService()).initialize();
+    final connectivity = GetIt.instance.get<ConnectivityService>();
     final apiClient = GetIt.instance.registerSingleton(
-      ApiClient(dio, connectivityService),
+      ApiClient(dio, connectivity),
     );
     GetIt.instance.registerFactory<CountryRepository>(
       () => CountryRepositoryImpl(apiClient),
     );
-  }
-
-  Dio _createDio() {
-    final dio = Dio(BaseOptions(baseUrl: APIEndpoint.baseUrl));
-    dio.interceptors.add(HeaderInterceptor());
-    if (kDebugMode) {
-      dio.interceptors.add(
-        PrettyDioLogger(
-          requestHeader: true,
-          requestBody: true,
-          responseBody: true,
-          responseHeader: false,
-          error: true,
-          compact: true,
-          maxWidth: 90,
-        ),
-      );
-    }
-    return dio;
   }
 }
